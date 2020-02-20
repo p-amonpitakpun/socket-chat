82031fee5ae01ff71a3f7fff895fa87c656dd986
pawat <p.amonpitakpun@gmail.com>
2020-02-21 00:03:59 +0700

    Add Client class

--- chat-client/client.cpp
+++ chat-client/client.cpp
@@ -25,139 +25,162 @@ int iResult, iSendResult;
 struct addrinfo* result = NULL, * ptr = NULL, hints;
 SOCKET ConnectSocket = INVALID_SOCKET;
 
+int inputbuflen = DEFAULT_BUFLEN;
+char inputbuf[DEFAULT_BUFLEN];
 char err[100];
 
-int startClient() {
-	// Initialize Winsock
-	printf(">> Initialize Winsock... ");
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		sprintf_s(err, "getaddrinfo failed with error: %d\r\n", iResult);
+class Client {
+private:
+	char port[10] = "8080";
 
-		return 1;
-	}
-	printf("\t\tCOMPLETED\r\n");
+	int recvbuflen = DEFAULT_BUFLEN;
+	char recvbuf[DEFAULT_BUFLEN];
+	int sendbuflen = DEFAULT_BUFLEN;
+	char sendbuf[DEFAULT_BUFLEN] = "Hi";
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
+	WSADATA wsaData;
+	int iResult, iSendResult;
+	struct addrinfo* result = NULL, * ptr = NULL, hints;
+	SOCKET ConnectSocket = INVALID_SOCKET;
 
-	// Get address information
-	printf(">> Get address information... ");
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+	int inputbuflen = DEFAULT_BUFLEN;
+	char inputbuf[DEFAULT_BUFLEN];
+	char err[100];
 
-	iResult = getaddrinfo("127.0.0.1", PORT, &hints, &result);
-	if (iResult != 0) {
-		sprintf_s(err, "getaddrinfo failed: %d\r\n", iResult);
-		WSACleanup();
+	char prefix = '/';
 
-		return 1;
-	}
-	printf("\t\tCOMPLETED\r\n");
+public:
+	Client() {
 
-	// Attempt to connect to an address
-	printf(">> Attempt to connect... \r\n");
-	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+		// Initialize Winsock
+		printf(">> Initialize Winsock... ");
+		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (iResult != 0) {
+			sprintf_s(err, "getaddrinfo failed with error: %d\r\n", iResult);
 
-		// Create a SOCKET for connecting to server
-		printf(">> ---- Create a SOCKET...");
-		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-		if (ConnectSocket == INVALID_SOCKET) {
-			sprintf_s(err, "socket failed with error: %d\r\n", WSAGetLastError());
+			throw new std::exception(err);
+		}
+		printf("\t\tCOMPLETED\r\n");
+
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+
+		// Get address information
+		printf(">> Get address information... ");
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+		hints.ai_flags = AI_PASSIVE;
+
+		iResult = getaddrinfo("127.0.0.1", port, &hints, &result);
+		if (iResult != 0) {
+			sprintf_s(err, "getaddrinfo failed: %d\r\n", iResult);
 			WSACleanup();
 
-			return 1;
+			throw new std::exception(err);
 		}
 		printf("\t\tCOMPLETED\r\n");
 
-		printf(">> ---- Connect to server... ");
-		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	}
+
+	void run() {
+
+		// Attempt to connect to an address
+		printf(">> Attempt to connect... \r\n");
+		for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+			// Create a SOCKET for connecting to server
+			printf(">> ---- Create a SOCKET...");
+			ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+			if (ConnectSocket == INVALID_SOCKET) {
+				sprintf_s(err, "socket failed with error: %d\r\n", WSAGetLastError());
+				WSACleanup();
+
+				throw new std::exception(err);
+			}
+			printf("\t\tCOMPLETED\r\n");
+
+			printf(">> ---- Connect to server... ");
+			iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+			if (iResult == SOCKET_ERROR) {
+				printf("\t\tFAILED!\r\n");
+				closesocket(ConnectSocket);
+				ConnectSocket = INVALID_SOCKET;
+
+				continue;
+			}
+			printf("\t\tCOMPLETED\r\n");
+			break;
+		}
+		freeaddrinfo(result);
+		printf(">> connected to server\r\n");
+
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("ERROR INVALID SOCKET\r\n");
+			WSACleanup();
+
+			throw new std::exception(err);
+		}
+
+
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("ERROR INVALID SOCKET\r\n");
+			WSACleanup();
+
+			throw new std::exception(err);
+		}
+
+		int isRunning = 1;
+		do {
+			printf(">> ");
+			std::cin >> inputbuf;
+
+			if (inputbuf[0] == prefix) {
+				if (strcmp(inputbuf, "/exit") == 0) {
+					isRunning = 0;
+				}
+			}
+			else {
+				send(ConnectSocket, inputbuf, strlen(inputbuf), 0);
+			}
+		} while (isRunning);
+
+		// shutdown
+		printf(">> Shutdown the SOCKET... ");
+		iResult = shutdown(ConnectSocket, SD_SEND);
 		if (iResult == SOCKET_ERROR) {
-			printf("\t\tFAILED!\r\n");
+			sprintf_s(err, "shutdown failed: %d\r\n", WSAGetLastError());
 			closesocket(ConnectSocket);
-			ConnectSocket = INVALID_SOCKET;
+			WSACleanup();
 
-			continue;
+			throw new std::exception(err);
 		}
 		printf("\t\tCOMPLETED\r\n");
-		break;
-	}
-	freeaddrinfo(result);
-	printf(">> connected to server\r\n");
 
-	if (ConnectSocket == INVALID_SOCKET) {
-		printf("ERROR INVALID SOCKET\r\n");
-		WSACleanup();
-
-		return 1;
-	}
-	/*
-	iResult = send(ConnectSocket, sendbuf, (int)sendbuflen, 0);
-	if (iResult == SOCKET_ERROR) {
-		sprintf_s(err, "send failed: %d\r\n", WSAGetLastError());
+		// cleanup
 		closesocket(ConnectSocket);
 		WSACleanup();
-
-		return 1;
+		return;
 	}
-
-	printf("\r\n>> Byte Sent: %ld\n", iResult);
-	printf(">> Sent: ");
-	printf(sendbuf);
-	printf("\r\n");
-	*/
-
-
-	do {
-		iResult = recv(ConnectSocket, recvbuf, (int)recvbuflen, 0);
-		if (iResult > 0) {
-			printf(">> Byte received: %d\r\n", iResult);
-			printf(">> Received: ");
-			printf(recvbuf);
-			printf("\r\n");
-		}
-		else if (iResult == 0) {
-			printf("Connection closed\r\n");
-		}
-		else {
-			printf("recv failed: %d\r\n", WSAGetLastError());
-		}
-	} while (iResult > 0);
-
-	// shutdown
-	printf(">> Shutdown the SOCKET... ");
-	iResult = shutdown(ConnectSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		sprintf_s(err, "shutdown failed: %d\r\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-
-		return 1;
-	}
-	printf("\t\tCOMPLETED\r\n");
-
-	// cleanup
-	closesocket(ConnectSocket);
-	WSACleanup();
-	return 0;
-}
+};
 
 int main()
 {
 	printf("/* --- Socket Chat Client --- */\r\n");
 
-	if (startClient() != 0){
-		printf(err);
+	try {
+		Client client;
+		client.run();
+	}
+	catch (std::exception & e) {
+		printf(e.what());
 
 		printf("press KEY to exit.");
 		std::cin.sync();
 		std::cin.get();
-
 		return 1;
 	}
 
